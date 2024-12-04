649462e0beb3a3c382ee1df76e05b5bd67b25799
aSumo <1xtelescope@gmail.com>
2024-12-05 08:50:46 +0900

    add: Enhanced Readabilitiesを導入

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -8,9 +8,25 @@ import {
 } from '@nolebase/vitepress-plugin-git-changelog/client'
 
 import '@nolebase/vitepress-plugin-git-changelog/client/style.css'
+import { 
+    NolebaseEnhancedReadabilitiesMenu, 
+    NolebaseEnhancedReadabilitiesScreenMenu, 
+} from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
+
+import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css'
 
 const Theme: ThemeConfig = {
     extends: DefaultTheme,
+
+    Layout: () => {
+            return h(DefaultTheme.Layout, null, {
+            // A enhanced readabilities menu for wider screens
+            'nav-bar-content-after': () => h(NolebaseEnhancedReadabilitiesMenu), 
+            // A enhanced readabilities menu for narrower screens (usually smaller than iPad Mini)
+            'nav-screen-content-after': () => h(NolebaseEnhancedReadabilitiesScreenMenu), 
+        })
+    },
+    
     enhanceApp({ app }) {
         enhanceAppWithTabs(app)
         app.use(NolebaseGitChangelogPlugin)
--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -6,6 +6,20 @@ import {
 
 export default defineConfig(() => {
     return {
+        optimizeDeps: {
+            exclude: [ 
+                '@nolebase/vitepress-plugin-enhanced-readabilities/client', 
+                'vitepress', 
+                '@nolebase/ui', 
+            ], 
+        },
+        ssr: { 
+            noExternal: [ 
+                // If there are other packages that need to be processed by Vite, you can add them here.
+                '@nolebase/vitepress-plugin-enhanced-readabilities', 
+                '@nolebase/ui', 
+            ], 
+        }, 
         plugins: [ 
             GitChangelog({ 
                 repoURL: () => 'https://github.com/nolebase/integrations',
